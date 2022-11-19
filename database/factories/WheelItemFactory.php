bbe7cb7219599933e03fa3a71bc674f0fd7f97a1
Justin Telmo <justin.telmo@gmail.com>
2022-11-19 14:16:52 -0800

    Add model to WheelItemFactory

--- database/factories/WheelItemFactory.php
+++ database/factories/WheelItemFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\WheelItem;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class WheelItemFactory extends Factory
 {
+
+    protected $model = WheelItem::class;
     /**
      * Define the model's default state.
      *
